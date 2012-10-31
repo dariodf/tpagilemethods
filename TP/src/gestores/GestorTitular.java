e4579cd1c4f2e178dbb68faec7243078a5ddbf10
mrtnz.agustin@gmail.com <mrtnz.agustin@gmail.com@c12e3463-3b8d-df58-9b21-d767d426df20>
2012-10-31 16:01:34 +0000

    Creados métodos buscarContribuyente() en los gestores AdminBD y Titular

--- TP/src/gestores/GestorTitular.java
+++ TP/src/gestores/GestorTitular.java
@@ -1,5 +1,10 @@
 package gestores;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import entidades.Contribuyente;
+
 public class GestorTitular {
 
 	protected GestorTitular(){}
@@ -7,5 +12,17 @@ public class GestorTitular {
 	// Metodo encargado de devolver el singleton
 	public static GestorTitular getIstance(){
 		return instancia;
+	}
+	
+	private Contribuyente buscarContribuyente(String tipoDoc, String numDoc) throws SQLException{
+		ResultSet rs = AdminBD.getIstance().buscarContribuyente(tipoDoc, numDoc);
+		if(!rs.next()){}
+			//lanzar ventana
+		
+		Contribuyente contribuyente = new Contribuyente(rs.getString("nombre"), rs.getString("apellido"), rs.getString("tipodoc"), rs.getString("numdoc"), rs.getDate("fechanacimiento"), rs.getString("direccion"), rs.getString("factorrh"), rs.getBoolean("donante"), rs.getString("observaciones"), rs.getString("localidad"));
+		
+		return contribuyente;
+		
+			
 	}
 }

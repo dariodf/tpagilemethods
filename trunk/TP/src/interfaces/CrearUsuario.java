dc332bc440cf94e9088be87220b926e067b6b616
tiago.utn@gmail.com <tiago.utn@gmail.com@c12e3463-3b8d-df58-9b21-d767d426df20>
2012-11-07 16:03:57 +0000

    se pasa el manejo de la excepcion sql que puede tirar getIdUsuarioDispinible() al gestorUsuario

--- trunk/TP/src/interfaces/CrearUsuario.java
+++ trunk/TP/src/interfaces/CrearUsuario.java
@@ -16,35 +16,25 @@ import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.sql.SQLException;
+
 import gestores.GestorUsuario;
 import excepciones.GeneralException;
 
-public class CrearUsuario extends JDialog {
 
+
+public class CrearUsuario extends JDialog 
+{
 	private final JPanel contentPanel = new JPanel();
 	private JTextField txtUsuariox;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		try {
-			CrearUsuario dialog = new CrearUsuario();
-			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-			dialog.setVisible(true);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
+	
+	int idUsuarioDisponible = GestorUsuario.getIstance().getIdUsuarioDisponible();
+	
 	/**
 	 * Create the dialog.
 	 */
-	public CrearUsuario() {
-		
-				
-		
-		//super(new JFrame(),true);
+	public CrearUsuario() 
+	{
 		setResizable(false);
 		setTitle("Crear Usuario");
 		setBounds(100, 100, 290, 182);
@@ -52,6 +42,16 @@ public class CrearUsuario extends JDialog {
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
+		
+		txtUsuariox = new JTextField();
+		txtUsuariox.setHorizontalAlignment(SwingConstants.CENTER);
+		txtUsuariox.setEditable(false);
+		txtUsuariox.setBounds(101, 22, 122, 20);
+		contentPanel.add(txtUsuariox);
+		txtUsuariox.setColumns(10);
+		
+		//Muestra el número de usuario disponible cuando se crea la ventana
+		txtUsuariox.setText("Usuario-"+idUsuarioDisponible);
 		
 		final JComboBox comboBox = new JComboBox();
 		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Simple", "Administrador"}));
@@ -61,35 +61,47 @@ public class CrearUsuario extends JDialog {
 		
 		
 		JButton btnAceptar = new JButton("Aceptar");
-		btnAceptar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				try {
-					
+		btnAceptar.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				try 
+				{
 					boolean superUsuario=false;
 					if(comboBox.getSelectedIndex()==1)
 						superUsuario=true;
 						
-					String pass = GestorUsuario.getIstance().crearUsuario(superUsuario, "Usuario-"+GestorUsuario.getIstance().getIdUsuarioDisponible());
+					String password = GestorUsuario.getIstance().crearUsuario(superUsuario, "Usuario-"+idUsuarioDisponible);
+					dispose();
 					
-					JOptionPane.showMessageDialog(null, "Se ha creado con Exito el usuario /n"+"Usuario-"+GestorUsuario.getIstance().getIdUsuarioDisponible()+" Contraseña: "+pass, "Informacion",JOptionPane.INFORMATION_MESSAGE);
-					
-					
-				} catch (GeneralException e) {
+					JOptionPane.showMessageDialog(null, "Se ha creado con Exito el usuario \n"+"Usuario-"+idUsuarioDisponible+" Contraseña: "+password, "Informacion",JOptionPane.INFORMATION_MESSAGE);
+									
+				} 
+				catch (SQLException e) 
+				{
+					JOptionPane.showMessageDialog(null,e.getMessage() , "Error en la Base de Datos",JOptionPane.ERROR_MESSAGE);
+				}
+				catch (GeneralException e) 
+				{
 					e.lanzarMensaje("Error");
 				}
 				
 			}
 		});
+		
 		btnAceptar.setBounds(22, 113, 83, 23);
 		btnAceptar.setActionCommand("OK");
 		contentPanel.add(btnAceptar);
 	
 		JButton btnCancelar = new JButton("Cancelar");
-		btnCancelar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		btnCancelar.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
 				dispose();
 			}
 		});
+		
 		btnCancelar.setBounds(169, 113, 90, 23);
 		btnCancelar.setActionCommand("Cancel");
 		contentPanel.add(btnCancelar);
@@ -103,16 +115,6 @@ public class CrearUsuario extends JDialog {
 		lblPrivilegio.setBounds(22, 61, 69, 14);
 		contentPanel.add(lblPrivilegio);
 		
-		txtUsuariox = new JTextField();
-		txtUsuariox.setHorizontalAlignment(SwingConstants.CENTER);
-		txtUsuariox.setEditable(false);
-		txtUsuariox.setBounds(101, 22, 122, 20);
-		contentPanel.add(txtUsuariox);
-		txtUsuariox.setColumns(10);
-		
-		//MUESTRA EL NUMERO DE USUARIO DISPONIBLE
-		txtUsuariox.setText("Usuario-"+GestorUsuario.getIstance().getIdUsuarioDisponible());
-		
-		
+			
 	}
 }

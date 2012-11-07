506c7475079ebd399dda99499a2fb7430529bff1
mrtnz.agustin@gmail.com <mrtnz.agustin@gmail.com@c12e3463-3b8d-df58-9b21-d767d426df20>
2012-11-07 14:38:20 +0000

    Gestor Titular - Comentadas las funcionalidades.

--- TP/src/gestores/GestorTitular.java
+++ TP/src/gestores/GestorTitular.java
@@ -21,18 +21,17 @@ public class GestorTitular {
 		return instancia;
 	}
 	
+	// Busca un contribuyente en la base de datos y lo retorna
 	public Contribuyente buscarContribuyente(String tipoDoc, String numDoc) throws SQLException, GeneralException{
+		// Realiza la busqueda del contribuyente en la base de datos
 		ResultSet rs = AdminBD.getIstance().buscarContribuyente(tipoDoc, numDoc);
+		// Si no encuentra el contribuyente, lanza una excepcion
 		if(!rs.next()){
 			throw new GeneralException("No se encontro un contribuyente con el tipo y numero de documento ingresado.");
 		}
-			
-		
-		Contribuyente contribuyente = new Contribuyente(rs.getString("nombre"), rs.getString("apellido"), rs.getString("tipodoc"), rs.getString("numdoc"), rs.getDate("fechanacimiento"), rs.getString("direccion"), rs.getString("gruposanguineo"),rs.getString("factorrh"), rs.getBoolean("donante"), rs.getString("localidad"));
-		
+		// Instancia el contribuyente y lo retorna
+		Contribuyente contribuyente = new Contribuyente(rs.getString("nombre"), rs.getString("apellido"), rs.getString("tipodoc"), rs.getString("numdoc"), rs.getString("sexo"), rs.getString("estadocivil"), rs.getDate("fechanacimiento"), rs.getString("direccion"), rs.getString("gruposanguineo"),rs.getString("factorrh"), rs.getBoolean("donante"), rs.getString("localidad"));
 		return contribuyente;
-		
-			
 	}
 	
 

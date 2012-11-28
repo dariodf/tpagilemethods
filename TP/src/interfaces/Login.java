cb4da8bba982450560258e3da3b0d5ade83ff85d
mrtnz.agustin@gmail.com <mrtnz.agustin@gmail.com@c12e3463-3b8d-df58-9b21-d767d426df20>
2012-11-28 13:17:13 +0000

    Cambiadas algunas cuestiones esteticas del lanzamiento y agregado el lanzamiento de la PantallaPrincipal.

--- TP/src/interfaces/Login.java
+++ TP/src/interfaces/Login.java
@@ -7,10 +7,17 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import java.awt.Font;
 import javax.swing.border.EtchedBorder;
+
+import excepciones.GeneralException;
+
+import java.awt.Dialog.ModalityType;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Login extends JFrame {
 
@@ -26,6 +33,7 @@ public class Login extends JFrame {
 			public void run() {
 				try {
 					Login frame = new Login();
+					frame.setLocationRelativeTo(null);
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -43,41 +51,55 @@ public class Login extends JFrame {
 		this.setUndecorated(true);
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 322, 200);
+		setBounds(500, 280, 322, 200);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EtchedBorder(EtchedBorder.RAISED, null, null));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JButton btnNewButton = new JButton("Iniciar");
-		btnNewButton.setBounds(57, 140, 74, 30);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e)  {
+				// Comprobaciones
+				PantallaPrincipal pantallaPrincipal = new PantallaPrincipal();
+				pantallaPrincipal.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+				pantallaPrincipal.setVisible(true);
+				dispose();
+			}
+		});
+		btnNewButton.setBounds(40, 140, 85, 30);
 		contentPane.add(btnNewButton);
 		
 		JLabel lblNewLabel = new JLabel("Usuario");
-		lblNewLabel.setBounds(57, 64, 63, 14);
+		lblNewLabel.setBounds(40, 61, 80, 14);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblContrasea = new JLabel("Contrase\u00F1a");
-		lblContrasea.setBounds(57, 97, 63, 14);
+		lblContrasea.setBounds(40, 94, 80, 14);
 		contentPane.add(lblContrasea);
 		
 		textField = new JTextField();
-		textField.setBounds(130, 61, 130, 20);
+		textField.setBounds(149, 55, 130, 20);
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
 		textField_1 = new JTextField();
-		textField_1.setBounds(130, 94, 130, 20);
+		textField_1.setBounds(149, 88, 130, 20);
 		textField_1.setColumns(10);
 		contentPane.add(textField_1);
 		
 		JButton btnCancelar = new JButton("Cancelar");
-		btnCancelar.setBounds(186, 140, 74, 30);
+		btnCancelar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
+		btnCancelar.setBounds(194, 140, 85, 30);
 		contentPane.add(btnCancelar);
 		
 		JLabel lblNewLabel_1 = new JLabel("Inicio de Sesi\u00F3n");
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblNewLabel_1.setBounds(110, 26, 114, 14);
+		lblNewLabel_1.setBounds(118, 26, 130, 14);
 		contentPane.add(lblNewLabel_1);
 	}
 }
